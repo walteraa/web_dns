699038d8fb06b906cc691937eef45e388a417523
Walter Alves <walter.arruda.alves@gmail.com>
2020-01-06 15:35:31 -0300

    Add IP validation
    - Add validation in model fixing the rspec

--- app/models/dns_record.rb
+++ app/models/dns_record.rb
@@ -1,2 +1,7 @@
+# frozen_string_literal: true
+
+require 'resolv'
+
 class DnsRecord < ApplicationRecord
+  validates :ip_address, format: { with: Resolv::IPv4::Regex }
 end
--- spec/factories/ip_entry_factory.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-FactoryBot.define do
-  factory :ip_entry do
-    address { Faker::Internet.public_ip_v4_address }
-  end
-end
