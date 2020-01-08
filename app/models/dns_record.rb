bc949036b677164430c31d95f646e59c894c40dc
Walter Alves <walter.arruda.alves@gmail.com>
2020-01-08 11:12:04 -0300

    Remove unused stuffs

--- app/models/dns_record.rb
+++ app/models/dns_record.rb
@@ -6,7 +6,6 @@ class DnsRecord < ApplicationRecord
   validates :ip_address, format: { with: Resolv::IPv4::Regex }
   validates :ip_address, uniqueness: true
   has_and_belongs_to_many :hostnames, -> { distinct }, class_name: 'RelatedHostname'
-  accepts_nested_attributes_for :hostnames
 
   alias_attribute :ip, :ip_address
 end
